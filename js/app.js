6f1e058943994f807ae53bf5f3e5b865cfed1a60
iyanu aransiola <aransiolaiy@gmail.com>
2021-07-29 08:48:35 +0100

    initial inputs

---
+++ js/app.js
@@ -0,0 +1,22 @@
+let movieOne = document.querySelector("#movieOne");
+let movieTwo = document.querySelector("#movieTwo");
+let movieThree = document.querySelector("#movieThree");
+let movieFour = document.querySelector("#movieFour");
+let movieFive = document.querySelector("#movieFive");
+let movieSix = document.querySelector("#movieSix");
+let movieSeven = document.querySelector("#movieSeven");
+let movieEight = document.querySelector("#movieEight");
+
+
+movieOne.addEventListener("click", displayMovieInfo1);
+movieTwo.addEventListener("click", displayMovieInfo2);
+movieThree.addEventListener("click", displayMovieInfo3);
+movieFour.addEventListener("click", displayMovieInfo4);
+movieFive.addEventListener("click", displayMovieInfo5);
+movieSix.addEventListener("click", displayMovieInfo6);
+movieSeven.addEventListener("click", displayMovieInfo7);
+movieEight.addEventListener("click", displayMovieInfo8);
+
+//function displayMovieInfo1(){
+   // window.open("movieInfo1.html", "I Am Not Your Negro", "width=400, height=600, scrollbars=yes");
+//}
